cbb2a6af4243613150a38358fafdcb524077bc23
Eiichi Sato <sato.eiichi@gmail.com>
2014-04-01 05:59:23 +0900

    start work on the next version

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import find_packages
 from setuptools import Extension
 
 setup(name='pigrate',
-      version='0.0.1',
+      version='develop',
       description='Database schema migration tool written in Python',
       author='Eiichi Sato',
       author_email='sato.eiichi@gmail.com',
       url='https://github.com/eiiches/pigrate',
-      download_url='https://github.com/eiiches/pigrate/archive/v0.0.1.tar.gz',
+      download_url='https://github.com/eiiches/pigrate/archive/develop.tar.gz',
       keywords=['database', 'schema', 'migration'],
       requires=[],
       packages=['pigrate'],
