2eef85b8d5aff8b2d3f2f27ff225100d29a2649e
Eiichi Sato <sato.eiichi@gmail.com>
2014-04-19 04:02:11 +0900

    start work on the next version

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup
 
 
 setup(name='pigrate',
-      version='0.0.2',
+      version='develop',
       description='Database schema migration tool written in Python',
       author='Eiichi Sato',
       author_email='sato.eiichi@gmail.com',
       url='https://github.com/eiiches/pigrate',
-      download_url='https://github.com/eiiches/pigrate/archive/v0.0.2.tar.gz',
+      download_url='https://github.com/eiiches/pigrate/archive/develop.tar.gz',
       keywords=['database', 'schema', 'migration'],
       requires=[],
       packages=['pigrate'],
